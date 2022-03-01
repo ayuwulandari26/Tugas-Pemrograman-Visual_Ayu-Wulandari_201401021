df2087e95bd03766370dbb60927e344eaabca084
ayuwulandari26 <85831120+ayuwulandari26@users.noreply.github.com>
2022-03-01 22:05:11 +0700

    Add files via upload

---
+++ Tugas1.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: HP
+ * Date: 24/02/2022
+ * Time: 19:05
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Tugas1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue. . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ Tugas2.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: HP
+ * Date: 24/02/2022
+ * Time: 19:26
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Tugas2
+{
+	class ProgramHw
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Nama Kamu Siapa ?");
+			string nama = Console.ReadLine();
+			Console.WriteLine("Nama Kamu "+nama);
+		
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
